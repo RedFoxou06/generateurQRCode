52a5ed9971f1e7b02fbc7121e8cdce1801919100
Nathan Fraysse <nathan_fraysse@ens.univ-artois.fr>
2026-02-02 17:10:24 +0100

    Enregistrement direct dans les téléchargements

--- generateurQRCode.py
+++ generateurQRCode.py
@@ -1,7 +1,18 @@
 import qrcode
+from pathlib import Path
 
-donnee=input("Entrez le lien :  (ne pas rentrer le https://)")
-donnee="https://" + donnee
-img=qrcode.make(donnee)
-temp=donnee.split("/")
-img.save(f"{temp[2]}.png")
+donnee = input("Entrez le lien (sans https://) : ")
+full_url = "https://" + donnee
+
+download_path = Path.home() / "Downloads"
+
+temp = full_url.split("/")
+filename = f"{temp[2]}.png"
+
+final_file_path = download_path / filename
+
+img = qrcode.make(full_url)
+img.save(final_file_path)
+
+print(f"---")
+print(f"QRCode se trouve : {final_file_path}")
